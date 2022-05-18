38b102e22347884769232b2c8161994e07a20da7
Ana Carolina <carolinacanari2@gmail.com>
2022-05-18 10:25:47 -0300

    fisrt commit with java

---
+++ src/br/com/dio/FirstProgram.java
@@ -0,0 +1,20 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+	
+		Gato gato = new Gato ();
+		
+		System.out.println(gato);
+	
+		
+		/*int a = 2020;
+		int b = 2;
+		System.out.println("Hello World! " + (a + b));*/
+
+	}
+
+}
